d91a82935a4ecfd2343856c1d2f866efcac8a15a
Gregory Thomas <gprthomas@gmail.com>
2018-06-26 19:33:39 +0000

    Created SimpleTest, improved test ouputs

--- simpletest.js
+++ simpletest.js
@@ -37,24 +37,47 @@
  * -Joe Walnes
  * MIT License. See https://github.com/joewalnes/jstinytest/
  */
-var TinyTest = {
+ 
+ // DONE: Get success to be green.
+ // DONE: One error to console person failure
+ // DONE: Make failures red
+ // DONE: Show stack traces for failures
+ // DONE: Only show stack traces if you click expand
+ // DONE: Output summary statistics to the DOM
+
+let SimpleTestHelper = {
+  renderStats(tests, failures) {
+    let numberOfTests = Object.keys(tests).length;
+    let successes = numberOfTests - failures;
+    let summaryString = 'Ran ' + numberOfTests + " tests: " + successes + " successes, " + failures + " failures";
+    let outputElement = document.createElement('h1');
+    outputElement.textContent = summaryString;
+    document.body.appendChild(outputElement);
+    
+  }
+}
+
+let SimpleTest = {
 
     run: function(tests) {
-        var failures = 0;
+        let failures = 0;
+        
         for (var testName in tests) {
             var testAction = tests[testName];
             try {
                 testAction.apply(this);
-                console.log('Test:', testName, 'OK');
+                console.log('%c%s', 'color: green;',testName);
             } catch (e) {
                 failures++;
-                console.error('Test:', testName, 'FAILED', e);
+                console.groupCollapsed('%c%s', 'color: red;', testName);
                 console.error(e.stack);
+                console.groupEnd();
             }
         }
         setTimeout(function() { // Give document a chance to complete
             if (window.document && document.body) {
                 document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+                SimpleTestHelper.renderStats(tests, failures);
             }
         }, 0);
     },
@@ -89,10 +112,11 @@ var TinyTest = {
 
 };
 
-var fail               = TinyTest.fail.bind(this),
-    assert             = TinyTest.assert.bind(this),
-    assertEquals       = TinyTest.assertEquals.bind(this),
-    eq                 = TinyTest.assertEquals.bind(this), // alias for assertEquals
-    assertStrictEquals = TinyTest.assertStrictEquals.bind(this),
-    tests              = TinyTest.run.bind(this),
-    noteq              = TinyTest.assertNotEquals.bind(this);
+var fail               = SimpleTest.fail.bind(this),
+    assert             = SimpleTest.assert.bind(this),
+    assertEquals       = SimpleTest.assertEquals.bind(this),
+    eq                 = SimpleTest.assertEquals.bind(this), // alias for assertEquals
+    assertStrictEquals = SimpleTest.assertStrictEquals.bind(this),
+    tests              = SimpleTest.run.bind(this),
+    noteq              = SimpleTest.assertNotEquals.bind(this),
+    seq                = SimpleTest.assertStrictEquals.bind(this);
